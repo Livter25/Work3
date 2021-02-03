ac39fbd7d405ea2e732474c464232e2c461caf18
Pasha <bl4ckbeer@yandex.ru>
2021-02-03 11:32:13 +0500

    03022021

--- js/main.js
+++ js/main.js
@@ -11,5 +11,62 @@ $(document).ready(function () {
         }
 
     };
+    
+    $('.question__form').validate({
+
+      errorClass: "invalid",
+      errorElement: "em",
+      rules: {
+      // simple rule, converted to {required:true}
+      userName: {
+          required: true,
+          minlength: 2,
+          maxlength: 15,
+          
+      },
+      userMasange: "required",
+      userPhone: "required",
+      // compound rule
+      userEmail: {
+          required: true,
+          email: true
+      }
+      },
+      messages: {
+      userName:{
+          required: "Имя обязательно ",
+          minlength: jQuery.validator.format("Имя не короче {0} букв "),
+          maxlength: jQuery.validator.format("Имя не длинее {0} букв ")
+      } ,
+      userMasange:{
+          required: "Сообщение обязательно",
+      },
+      userEmail: {
+          required: "Обязателно укажите Email",
+          email: "Введите в формате sdfsd@asdasd.com"
+          },
+      userPhone:{
+          required: "Телефон обязательно",
+          minlength: jQuery.validator.format("Номер не короче {0} цифр "),
+          },
+      }
+  
+      });
+    
+    $('[type=tel]').mask('+7(000)-000-00-00', {placeholder: "Phone"});
+    
+    jQuery(function($){
+        $('a[href*="#"]').on('click.smoothscroll', function( e ){
+        var hash    = this.hash, _hash   = hash.replace(/#/,''), theHref = $(this).attr('href').replace(/#.*/, '');
+        if( theHref && location.href.replace(/#.*/,'') != theHref ) return;
+        var $target = _hash === '' ? $('body') : $( hash + ', a[name="'+ _hash +'"]').first();
+        if( ! $target.length ) return;
+        e.preventDefault();
+        $('html, body').stop().animate({ scrollTop: $target.offset().top - 0 }, 900, 'swing', function(){
+        window.location.hash = hash;
+        });
+        });
+        });
+
           
 });
